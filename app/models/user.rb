04baf6d24cbb84203ab171913371978698147e5c
m-kanemaru <m.kanemaru.1059@gmail.com>
2023-01-06 12:58:17 +0000

    ユーザーコントローラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one_attached :image_id
+  def get_image_id
+    (image_id.attached?) ? image_id : 'no_image.jpg'
+  end
 end
