16b0580a3f71abeff789e0c6e43e269c2286d86d
m-kanemaru <m.kanemaru.1059@gmail.com>
2023-01-11 12:50:27 +0000

    コントローラー追記

--- app/controllers/publics/groups_controller.rb
+++ app/controllers/publics/groups_controller.rb
@@ -1,2 +1,73 @@
 class Publics::GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def new
+    @group = Group.new
+  end
+  
+  def index
+    # @groups = Group.order("group.user_groups.count(user_id) desc").page(params[:page])
+    groups =  Group.includes(:user_groups).sort {|a,b| b.user_groups.size <=> a.user_groups.size}
+    @groups = Kaminari.paginate_array(groups).page(params[:page])
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @users = @group.users.page(params[:page])
+  end
+    
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to  groups_path
+  end
+  
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    @group.users << current_user
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
+  end
+  
+  def all_destroy
+    @group = Group.find(params[:group_id])
+    @group.destroy
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :introduction, :image_id)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
 end
